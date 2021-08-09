ac4f54dfe9b7e85304f5ea83a9f13abea0192f35
Thomas Ferreira Da Silva <thomas.ferreirads@outlook.fr>
2021-08-09 12:24:48 +0200

    Add settings updated files

---
+++ src/Settings/CmsSettings.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Settings;
+
+use App\Entity\CmsPages;
+use App\Entity\CmsSettings as EntityCmsSettings;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+class CmsSettings
+{
+    private $em;
+
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getGamePage()
+    {
+        $gamePage = $this->em->getRepository(CmsPages::class)->findBy(['category' => "game", 'isVisible' => 1]);
+        return $gamePage;
+    }
+
+    public function getWikiPage()
+    {
+        $gamePage = $this->em->getRepository(CmsPages::class)->findBy(['category' => "wiki", 'isVisible' => 1]);
+        return $gamePage;
+    }
+
+    public function get($param)
+    {
+        return $this->em->getRepository(EntityCmsSettings::class)->findOneBy(['setting' => $param])->getDefaultValue();
+    }
+
+    public function getCache($name){
+        $cache = new FilesystemAdapter();
+
+        return $cache->getItem($name)->get();
+    }
+}
